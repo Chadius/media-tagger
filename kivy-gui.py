8c8952c910325345ae33edd63feaeeeb2dc5e028
Chad Serrant <chadius@alum.mit.edu>
2018-05-23 20:30:49 -0400

    Created my first layout.

--- kivy-gui.py
+++ kivy-gui.py
@@ -2,9 +2,9 @@ import kivy
 kivy.require('1.10.0')
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 
-
-class TitleScreen(Widget):
+class TitleScreen(FloatLayout):
     pass
 
 
