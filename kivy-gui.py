0763431fde4d419e357cb99f739133e972cf0656
Chad Serrant <chadius@alum.mit.edu>
2018-05-23 20:15:08 -0400

    Adding sample UI layout.

--- kivy-gui.py
+++ kivy-gui.py
@@ -1,2 +1,17 @@
+import kivy
+kivy.require('1.10.0')
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class TitleScreen(Widget):
+    pass
+
+
+class UISwitcherApp(App):
+    def build(self):
+        return TitleScreen()
+
 if __name__ == '__main__':
-    print("Hello World")
+    UISwitcherApp().run()
+
