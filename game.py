c9d99855a3f7d18180521c26557f902be367aaf6
Chad Serrant <chadius@alum.mit.edu>
2018-07-04 20:22:22 -0400

    canvas-grid: Set up a drawing system.

--- game.py
+++ game.py
@@ -1,8 +1,21 @@
+from functools import partial
+
+from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
+from kivy.graphics import Color
+from kivy.graphics import Rectangle
 
 class GameScreen(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
+
+        Clock.schedule_interval(
+            partial(
+                GameScreen.update,
+                self
+            ),
+            1.0 / 30.0
+        )
 
     def switch_to_title_screen(self):
         """Close this widget and open the Title Screen.
@@ -11,5 +24,22 @@ class GameScreen(Screen):
         # Ask the parent to switch to the Game screen
         self.parent.change_scene("title_screen")
 
-    def on_touch_down(self, touch):
-        print(touch)
+    def update(self, dt):
+        # Stop if this window isn't active.
+        if not self.parent:
+            return
+
+        self.draw_tiles(dt)
+
+    def draw_tiles(self, dt):
+        """
+        """
+
+        # Stop if this window isn't active.
+        if not self.parent:
+            return
+
+        # Draw a box
+        with self.canvas:
+            Color(1.0, 0.0, 0.0)
+            Rectangle(pos=(740,20), size=(70, 220))
